0e1edbde7b2ca771ad3b15c2bb22a561d5f85293
correlator <scotthoch86@gmail.com>
2024-12-28 12:23:48 -0800

    cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from './components/Hero'
-import Services from './components/Services'
-import About from './components/About'
-import Team from './components/Team'
-import Contact from './components/Contact'
+import Hero from '@/components/Hero'
+import Services from '@/components/Services'
+import About from '@/components/About'
+import Team from '@/components/Team'
+import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
